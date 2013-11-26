8274b45eb5d7a75bc7816257f86a6846c47a86ea
Gulshan Singh <gulshan@umich.edu>
2013-11-25 23:36:53 -0500

    Added show IP address menu item

--- src/com/gulshansingh/ipscanner/MainActivity.java
+++ src/com/gulshansingh/ipscanner/MainActivity.java
@@ -3,6 +3,10 @@ package com.gulshansingh.ipscanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +18,7 @@ import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -48,6 +53,18 @@ public class MainActivity extends Activity {
 		case R.id.action_man_nmap:
 			Intent intent = new Intent(this, ManActivity.class);
 			startActivity(intent);
+			break;
+		case R.id.action_show_ip:
+			String ipAddress = getIpAddress();
+			AlertDialog.Builder b = new AlertDialog.Builder(this);
+			String message;
+			if (ipAddress != null) {
+				message = "Your IP address is " + ipAddress;
+			} else {
+				message = "Could not get IP address. Are you sure you're connected to a WiFi network?";
+			}
+			b.setTitle("IP Address").setMessage(message).create().show();
+			break;
 		}
 		return true;
 	}
@@ -172,5 +189,28 @@ public class MainActivity extends Activity {
 			sb.append(" ");
 		}
 		commandView.setText(sb.toString());
+	}
+
+	private String getIpAddress() {
+		WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+		int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+
+		// Convert little-endian to big-endian if needed
+		if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+			ipAddress = Integer.reverseBytes(ipAddress);
+		}
+
+		byte[] ipByteArray = BigInteger.valueOf(ipAddress).toByteArray();
+
+		String ipAddressString;
+		try {
+			ipAddressString = InetAddress.getByAddress(ipByteArray)
+					.getHostAddress();
+		} catch (UnknownHostException ex) {
+			Log.e("MainActivity", "Unable to get host address.");
+			ipAddressString = null;
+		}
+
+		return ipAddressString;
 	}
 }
